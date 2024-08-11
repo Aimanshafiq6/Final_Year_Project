db1bf52a3d8fc04daaf2232219737a1428ba495c
unknown <Zack2k2>
2024-08-12 03:09:30 +0500

    fixed the recomandation system

--- login.py
+++ login.py
@@ -1,17 +1,8 @@
 
-#import cv2
 import customtkinter as ctk
 from PIL import Image, ImageTk
-#from deepface import DeepFace
-#import numpy as np
-#import tests.text_sentiment_analysis as text_sentiment_analysis
-#from scipy.io.wavfile import write
-#import time
 import sounddevice as sd
-#import sys
 import os
-#import speech_recognition as sr
-#from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import json
 
 
@@ -69,33 +60,6 @@ class LoginWindow(ctk.CTk):
         signup_button = ctk.CTkButton(master=self.frame, text="guest session",command=self.open_bool_question_window, fg_color="#EEEEEE", hover_color="#EEEEEE", font=("Arial Bold", 9), text_color="#601E88", width=225, image=self.add_user_img).pack(anchor="w", pady=(20, 0), padx=(25, 0))
 
 
-        # # Create labels and entry widgets for username and password
-        # username_label = ctk.CTkLabel(self, text="Username")
-        # username_label.pack(pady=5)
-        # username_entry = ctk.CTkEntry(self)
-        # username_entry.pack(pady=5)
-
-        # password_label = ctk.CTkLabel(self, text="Password")
-        # password_label.pack(pady=5)
-        # password_entry = ctk.CTkEntry(self, show="*")
-        # password_entry.pack(pady=5)
-
-        # # # Create a label to show error messags
-        # error_label = ctk.CTkLabel(self, text="", fg_color="red")
-        # error_label.pack(pady=5)
-
-        # # Create a login button
-        # login_button = ctk.CTkButton(self,text="Login", command=lambda: self.validate_login(username_entry.get(), password_entry.get()))
-        # login_button.pack(pady=10)
-
-        # # create a sign up label
-        # signup_button = ctk.CTkButton(self, text="sign up", command=self.open_sign_up_wondow)
-        # signup_button.pack(pady=10)
-
-        # # Create a Guest Login in button
-        # guest_button = ctk.CTkButton(self, text="Guest User", command=self.open_bool_question_window)
-        # guest_button.pack(pady=10)
-
 
         
     # Function to validate the login credentials
@@ -113,7 +77,8 @@ class LoginWindow(ctk.CTk):
                 self.destroy()
                 print("Welcome!")
                 from main_app import app
-                window = app()
+                age = user.get("age")
+                window = app(age)
                 window.mainloop()
 
                 
@@ -124,7 +89,7 @@ class LoginWindow(ctk.CTk):
 
     def open_sign_up_window(self):
         self.destroy()
-        import subprocess
+        #import subprocess
         from signup_form import SignUpForm
         window = SignUpForm()
         window.run()
